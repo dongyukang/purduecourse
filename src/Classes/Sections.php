6ce574c8bf3659faaef998aba9d1403c4961d6ff
Dongyu Kang <dongyu@dongyukang.com>
2017-06-01 18:53:34 -0400

    section and it takes care when there are more than one course.

--- src/Classes/Sections.php
+++ src/Classes/Sections.php
@@ -8,18 +8,38 @@ class Sections extends Classes
 {
   protected $sections_info;
 
+  /**
+   * Get all sections from certain class.
+   */
   public function sections()
   {
-    $this->sections_info = $this->class_info['Sections'];
+    if ($this->countCourses() > 1) {
+      $this->sections_info = array();
+      foreach ($this->classes as $class) {
+        array_push($this->sections_info, $class['Sections']);
+      }
+    } else {
+      $this->sections_info = $this->class_info['Sections'];
+    }
 
     return $this;
   }
 
+  /**
+   * Get certain sections by filtering through condition.
+   *
+   * @param  $condition
+   */
   public function sectionByCondition($condition)
   {
 
   }
 
+  /**
+   * Get actual data of section.
+   *
+   * @return Array
+   */
   public function getSectionInfo()
   {
     return $this->sections_info;
